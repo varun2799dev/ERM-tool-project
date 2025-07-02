2cf7dec3ca7155feff4856740e0f045690ecbd0a
Varun Tadpatri <varuntadpatri@gmail.com>
2025-07-02 22:02:28 +0530

    Again made changes to CORS

--- Backend/server.js
+++ Backend/server.js
@@ -6,12 +6,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: 'https://erm-tool-project.vercel.app',
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://erm-tool-project.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.use(express.json());
 
